37d342d97ba7098707ce009415a92e91b7d9597e
audrey <audreyanggraini1@gmail.com>
2024-01-18 13:09:22 +0700

    second page

---
+++ lib/app/modules/home/views/home_view.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:peminjam_perpustakaan_app/app/modules/home/controllers/home_controller.dart';
+import 'package:peminjam_perpustakaan_app/app/routes/app_pages.dart';
+
+class HomeView extends GetView<HomeController> {
+  const HomeView({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('HomeView'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            ElevatedButton(onPressed: () => Get.toNamed(Routes.BOOK),
+                child: const Text("List Buku")),
+            ElevatedButton(onPressed: () => Get.toNamed(Routes.LOGIN),
+                child: const Text("List Pinjam"))
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/app/modules/login/controllers/login_controller.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/cupertino.dart';
+import 'package:dio/dio.dart 'as dio;
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:peminjam_perpustakaan_app/app/data/constant/endpoint.dart';
+import 'package:peminjam_perpustakaan_app/app/data/model/response_login.dart';
+import 'package:peminjam_perpustakaan_app/app/data/provider/api_provider.dart';
+import 'package:peminjam_perpustakaan_app/app/data/provider/storage_provider.dart';
+import 'package:peminjam_perpustakaan_app/app/routes/app_pages.dart';
+
+
+class LoginController extends GetxController {
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  final TextEditingController usernameController = TextEditingController();
+  final TextEditingController passwodController = TextEditingController();
+
+  //TODO: Implement LoginController
+
+  final count = 0.obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+  }
+
+  @override
+  void onReady() {
+    super.onReady();
+  }
+
+  @override
+  void onClose() {
+    super.onClose();
+  }
+
+  void increment() => count.value++;
+
+  final loadingLogin = false.obs;
+
+  login() async {
+    loadingLogin(true);
+    try {
+      //untuk tidak memunculkan keyboard
+      FocusScope.of(Get.context!).unfocus();
+      formKey.currentState?.save();
+      if (formKey.currentState!.validate()) {
+        final response = await ApiProvider.instance().post(Endpoint.login,
+            data: dio.FormData.fromMap(
+                {
+                  "username": usernameController.text.toString(),
+                  "password": passwodController.text.toString()
+                }));
+        if (response.statusCode == 200) {
+          final ResponseLogin responseLogin =
+          ResponseLogin.fromJson(response.data);
+          await StorageProvider.write(StorageKey.idUser,
+            "${responseLogin.data?.id}");
+          await StorageProvider.write(StorageKey.status, "logged");
+          Get.offAllNamed(Routes.HOME);
+        } else {
+          Get.snackbar("Sorry", "Login Gagal", backgroundColor: Colors.orange);
+        }
+      }
+      loadingLogin(false);
+    } on dio.DioException catch (e) {
+      loadingLogin(false);
+      Get.snackbar("Sorry", e.message ?? "", backgroundColor: Colors.red);
+    } catch (e) {
+      loadingLogin(false);
+      Get.snackbar("error", e.toString(), backgroundColor: Colors.red);
+      throw Exception('Error: $e');
+    }
+  }
+}
